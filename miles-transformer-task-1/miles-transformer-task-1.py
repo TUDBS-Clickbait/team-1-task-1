a94fdd7519662ab159f8fd1c0727286a01230fc6
Jakob Mueller <jakob.mueller@ibot3.de>
2022-11-04 14:55:28 +0100

    Add MILES transformer

---
+++ miles-transformer-task-1/miles-transformer-task-1.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+import argparse
+import json
+import pandas as pd
+import numpy as np
+from simpletransformers.classification import ClassificationModel
+import torch
+
+from simplifier import models
+from simplifier import config
+from simplifier import simplifier
+
+config.lang = "en"
+models.embeddings = models.load_embeddings(config.lang)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='This is a baseline for task 1 that predicts that each clickbait post warrants a passage spoiler.')
+
+    parser.add_argument('--input', type=str, help='The input data (expected in jsonl format).', required=True)
+    parser.add_argument('--output', type=str, help='The classified output in jsonl format.', required=False)
+
+    return parser.parse_args()
+
+
+def load_input(df):
+    if type(df) != pd.DataFrame:
+        df = pd.read_json(df, lines=True)
+    
+    ret = []
+    for _, i in df.iterrows():
+        ret += [{'text': ' '.join(i['postText']) + ' - ' + i['targetTitle'] + ' ' + ' '.join(i['targetParagraphs']), 'uuid': i['uuid']}]
+    
+    return pd.DataFrame(ret)
+
+
+def use_cuda():
+    return torch.cuda.is_available() and torch.cuda.device_count() > 0
+
+
+def predict(df):
+    df = load_input(df)
+    labels = ['phrase', 'passage', 'multi']
+    model = ClassificationModel('deberta', '/model', use_cuda=use_cuda())
+
+    simplified_text = simplifier.simplify_text(df['text'].rstrip("\n"))
+
+    uuids = list(df['uuid'])
+    texts = list(simplified_text)
+    predictions = model.predict(texts)[1]
+    
+    for i in range(len(df)):
+        yield {'uuid': uuids[i], 'spoilerType': labels[np.argmax(predictions[i])]}
+
+
+def run_baseline(input_file, output_file):
+    with open(output_file, 'w') as out:
+        for prediction in predict(input_file):
+            out.write(json.dumps(prediction) + '\n')
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    run_baseline(args.input, args.output)
+
